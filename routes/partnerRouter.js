fed069156084bd2a644489c3935e82f6c1c0c499
Aaron Shade <shadeam@gmail.com>
2021-07-01 18:19:49 -0400

    Passport JWT

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const express = require("express");
 const Partner = require("../models/partner");
 const partnerRouter = express.Router();
+const authenticate = require("../authenticate");
 
 partnerRouter
 	.route("/")
@@ -14,7 +15,7 @@ partnerRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		Partner.create(req.body)
 			.then((partner) => {
 				console.log("Partner Created ", partner);
@@ -24,11 +25,11 @@ partnerRouter
 			})
 			.catch((err) => next(err));
 	})
-	.put((req, res) => {
+	.put(authenticate.verifyUser, (req, res) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /partners");
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Partner.deleteMany()
 			.then((response) => {
 				res.statusCode = 200;
@@ -49,7 +50,7 @@ partnerRouter
 			})
 			.catch((err) => next(err));
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Partner.findOneAndDelete({ name: req.body.name })
 			.then((response) => {
 				res.statusCode = 200;
@@ -82,14 +83,14 @@ partnerRouter
 			.catch((err) => next(err));
 	})
 
-	.post((req, res) => {
+	.post(authenticate.verifyUser, (req, res) => {
 		res.statusCode = 403;
 		res.end(
 			`POST operation not supported on /partners/${req.params.partnerId}`
 		);
 	})
 
-	.put((req, res, next) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		Partner.findByIdAndUpdate(
 			req.params.partnerId,
 			{
@@ -105,7 +106,7 @@ partnerRouter
 			.catch((err) => next(err));
 	})
 
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Partner.findByIdAndDelete(req.params.partnerId)
 			.then((response) => {
 				res.statusCode = 200;
