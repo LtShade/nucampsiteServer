d25877971b6301e8283ffacf82b500b61a6d769f
Aaron Shade <shadeam@gmail.com>
2021-07-08 19:17:46 -0400

    oops

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	secretKey: "12345-67890-09876-54321",
-	mongoUrl: "mongodb://localhost:27017/nucampsite",
-	facebook: {
-		clientId: "651979966196346",
-		clientSecret: "d70b034ff39515585fc1b00c2d0515c6",
-	},
-};
